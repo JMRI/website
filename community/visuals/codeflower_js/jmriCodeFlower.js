9a4dea9d7ca8311329a042a6b61eb25ae4a2d0c4
Egbert Broerse <dcc2@ebroerse.nl>
2022-07-19 00:25:30 +0200

    add sidebars to visuals, clinics, gallery. Update CodeFlower 2022

---
+++ community/visuals/codeflower_js/jmriCodeFlower.js
@@ -0,0 +1,16 @@
+      var currentCodeFlower;
+      var createCodeFlower = function(json) {
+        // remove previous flower to save memory
+        if (currentCodeFlower) currentCodeFlower.cleanup();
+        // adapt layout size to the total number of elements
+        //var total = countElements(json);
+        w = 1500; //parseInt(Math.sqrt(total) * 5, 10);
+        h = 1500; //parseInt(Math.sqrt(total) * 5, 10);
+        // create a new CodeFlower
+        currentCodeFlower = new CodeFlower("#visualization", w, h).update(json);
+      };
+      d3.json('codeflower_js/java.json', createCodeFlower);
+
+      document.getElementById('part').addEventListener('change', function() {
+        d3.json(this.value, createCodeFlower);
+      });
