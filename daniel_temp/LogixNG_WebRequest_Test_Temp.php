6000e293f14f60b93089af6d634755f9b011d9cb
Daniel <db123@bergqvist.se>
2023-04-08 15:56:24 +0200

    Create LogixNG_WebRequest_Test_Temp.php

---
+++ daniel_temp/LogixNG_WebRequest_Test_Temp.php
@@ -0,0 +1,11 @@
+<?php
+
+// This php script is used to test and demonstrate the LogixNG action WebRequest.
+
+
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
+
+require_once("LogixNG_WebRequest_Test.php");
+
+?>
